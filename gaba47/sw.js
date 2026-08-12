2a65eedb140201c7de8a2d0583beeab701a52b8e
guhuding-lang <guhuding@gmail.com>
2026-08-13 03:16:43 +0800

    fix(gaba47): refresh page shell before using cache

--- gaba47/sw.js
+++ gaba47/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'gaba47-v4'
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest']
+const CACHE = 'gaba47-v5'
+const APP_SHELL = ['./index.html', './manifest.webmanifest']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)))
@@ -15,6 +15,21 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          if (response.ok) {
+            caches.open(CACHE).then((cache) => cache.put('./index.html', response.clone()))
+          }
+          return response
+        })
+        .catch(() => caches.match('./index.html')),
+    )
+    return
+  }
+
   event.respondWith(
     caches.match(event.request).then((cached) => {
       const network = fetch(event.request)
